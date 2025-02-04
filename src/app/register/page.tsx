e1428267a3184bf266a94e2091ddffd7f4b69b97
Gustavo Gantois <gugagantois@gmail.com>
2025-02-03 22:37:49 -0300

    feat:Creating new page and new component LoginForm.

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,8 +5,8 @@ import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 export default function Page() {
     return (
         <section className="mt-28 lg:mt-60 p-4">   
-            <Card>
-            <CardHeader className="flex items-center justify-center gap-6">
+            <Card className="w-full lg:w-1/2">
+            <CardHeader className="flex items-center justify-center gap-6"> 
                 <CardTitle className="text-white text-[32px] lg:text-[40px]">Void IA</CardTitle>
                 <Image src="/LogoIcon.svg" width={41} height={36} alt="" />
             </CardHeader>
