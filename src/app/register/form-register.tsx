38adca94549597f132a91f149975cb4e5a4f117d
Gustavo Gantois <gugagantois@gmail.com>
2025-02-03 22:47:14 -0300

    feat:Create new page and new component form-register.tsx.

--- src/app/register/form-register.tsx
+++ src/app/register/form-register.tsx
@@ -1,20 +1,9 @@
 "use client";
-import { z } from "zod"
-import { useToast } from "@/hooks/use-toast"
-import { Router } from "next/navigation"
-import { Form, FormField, FormControl, FormMessage, FormLabel, FormItem } from "@/components/ui/form"
-import { Button } from "@/components/ui/button"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Input } from "@/components/ui/input"
-import { Card, CardTitle, CardDescription, CardContent, CardHeader } from "@/components/ui/card"
 
 export default function FormRegister() {
-
-    
-    
     return (
-        <div>
+        <div className="text-white text-2xl text-center">
+            Formulário de Registro.
         </div>
     )
 }
