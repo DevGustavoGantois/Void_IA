affeaa752793b8b66eb5fe492a782c5cfc804973
Gustavo Gantois <gugagantois@gmail.com>
2025-02-10 20:26:40 -0300

    feat:Create new functionalitys, remove and adding questions.

--- src/app/platform/components/sidebar.tsx
+++ src/app/platform/components/sidebar.tsx
@@ -1,37 +1,59 @@
 import { LogOut, MessageCircle, Trash } from "lucide-react";
 import Link from "next/link";
 import { MenuMobile } from "./menu-mobile";
+import { useRouter } from "next/navigation";
 
-export function Sidebar() {
+interface Props {
+    questions: {id: string, text: string}[];
+    removeQuestion: (id: string) => void;
+}
+
+export function Sidebar({ questions, removeQuestion }: Props) {
+    const router = useRouter();
+
+    const goToQuestion = (id: string) => {
+        router.push(`/pergunta/${id}`);
+    };
+
     return (
         <aside>
-            <div className="hidden bg-gradient-to-r from-[#03083B] p-8 w-[20%] min-w-[250px] h-screen shadow-lg md:flex flex-col">
-            <div className="flex flex-col gap-8">
-                <div className="flex items-center justify-center">
-                    <div className="border border-white p-2 w-full rounded-lg">
-                        <p className="text-white text-center">Lista de Conversas</p>
+            <div className="hidden bg-gradient-to-r from-[#03083B] p-8 w-[100%] min-w-[250px] h-screen shadow-lg md:flex flex-col">
+                <div className="flex flex-col gap-8">
+                    <div className="flex items-center justify-center">
+                        <div className="border border-white p-2 w-full rounded-lg">
+                            <p className="text-white text-center">Lista de Conversas</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 overflow-y-auto">
+                        {questions.map((question: {id: string, text: string}) => (
+                            <div 
+                                key={question.id} 
+                                className="bg-[#1D1D1D] p-4 rounded-lg flex justify-between items-center cursor-pointer"
+                                onClick={() => goToQuestion(question.id)}
+                            >
+                                <MessageCircle className="text-white/50 transition-all duration-300 hover:text-emerald-500" />
+                                <p className="text-white">{question.text}</p>
+                                <Trash 
+                                    onClick={(e) => { 
+                                        e.stopPropagation();
+                                        removeQuestion(question.id);
+                                    }} 
+                                    className="text-white/50 cursor-pointer hover:text-red-500 transition-all duration-300" 
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="mt-60 flex items-center justify-center">
+                        <Link href="/" className="flex items-center text-xl gap-4">
+                            Sair
+                            <LogOut size={20} className="text-white" />
+                        </Link>
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 overflow-y-auto">
-                    {[...Array(6)].map((_, index) => (
-                        <div key={index} className="bg-[#1D1D1D] p-4 rounded-lg flex justify-between items-center">
-                            <MessageCircle className="text-white/50 transition-all duration-300 hover:text-emerald-500" />
-                            <p className="text-white">Pergunta</p>
-                            <Trash className="text-white/50 cursor-pointer hover:text-red-500 transition-all duration-300" />
-                        </div>
-                    ))}
-                </div>
-                <div className="mt-60 flex items-center justify-center">
-                    <Link href="/" className="flex items-center text-xl gap-4">
-                    Sair
-                    <LogOut size={20} className="text-white" />
-                    </Link>
-                </div>
-            </div>
             </div>
             <div className="flex lg:hidden">
                 <MenuMobile />
             </div>
         </aside>
-    )
+    );
 }
--- src/app/platform/page.tsx
+++ src/app/platform/page.tsx
@@ -1,32 +1,60 @@
+"use client";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Sidebar } from "./components/sidebar";
 import Image from "next/image";
 import { CircleArrowUp } from "lucide-react";
 
 export default function Page() {
+    const [questions, setQuestions] = useState([
+        { id: "1", text: "O que é Void IA?" },
+        { id: "2", text: "Como funciona o aprendizado de máquina?" },
+        { id: "3", text: "Qual é a diferença entre IA e ML?" },
+    ]);
+    
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const addQuestion = (questionText: string) => {
+        const newQuestion = { id: String(Date.now()), text: questionText };
+        setQuestions((prev) => [...prev, newQuestion]);
+    };
+
+    const removeQuestion = (id: string) => {
+        setQuestions((prev) => prev.filter((question) => question.id !== id));
+    };
+
     return (
         <section className="flex h-screen">
-            <Sidebar />
+            <Sidebar questions={questions} removeQuestion={removeQuestion} />
             <div className="flex flex-1 flex-col items-center justify-center gap-10 px-6">
                 <h1 className="text-center text-white font-medium text-2xl lg:text-4xl mb-60">Void IA</h1>
                 <div className="flex items-center gap-4">
-                 <Image src="/LogoIcon.svg" width={41} height={36} alt="" />
-                 <p className="text-white text-center text-xl">Olá Seja bem vindo ao Void IA</p>
+                    <Image src="/LogoIcon.svg" width={41} height={36} alt="" />
+                    <p className="text-white text-center text-xl">Olá, seja bem-vindo ao Void IA</p>
                 </div>
                 <div className="relative w-full max-w-4xl">
-            <Input
-                className="w-full text-center p-3 pr-12 rounded-lg shadow-md"
-                type="text"
-                placeholder="Digite sua dúvida aqui..."
-                id="text_gpt"
-                name="text_gpt"
-            />
-            <CircleArrowUp
-                className="absolute inset-y-0 right-3 my-auto text-gray-500 cursor-pointer hover:text-gray-700 transition"
-                size={24}
-            />
-        </div>
+                    <Input
+                        className="w-full text-center p-3 pr-12 rounded-lg shadow-md"
+                        type="text"
+                        placeholder="Digite sua dúvida aqui..."
+                        id="text_gpt"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        name="text_gpt"
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                addQuestion(e.currentTarget.value);
+                                e.currentTarget.value = ""; 
+                            }
+                        }}
+                    />
+                    <CircleArrowUp
+                        onClick={() => {addQuestion(inputValue)}}
+                        className="absolute inset-y-0 right-3 my-auto text-gray-500 cursor-pointer hover:text-gray-700 transition"
+                        size={24}
+                    />
+                </div>
             </div>
         </section>
-    )
+    );
 }
